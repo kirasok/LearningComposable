0505d6a0c9431e90340411c2a7b1618c7ef431c7
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2023-04-29 14:59:39 +0300

    feat(navigation): add destinations

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Learning Composable
 
 App where I try to learn [Jetpack Compose UI App Development Toolkit](https://developer.android.com/jetpack/compose)
+
+## Destinations
+
+- [Article](https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems#1)
+- [Task Manager](https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems#2)
+- [Quadrant](https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems#3)
--- app/src/main/java/io/github/kirasok/learningcomposable/MainActivity.kt
+++ app/src/main/java/io/github/kirasok/learningcomposable/MainActivity.kt
@@ -3,26 +3,44 @@ package io.github.kirasok.learningcomposable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import io.github.kirasok.learningcomposable.ui.theme.LearningComposableTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearningComposableTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "root") {
+                composable("root") {
+                    Root(
+                        { navController.navigate("article") },
+                        { navController.navigate("task-manager") },
+                        { navController.navigate("quadrant") }
+                    )
+                }
+                composable("article") {
+                    Article()
+                }
+                composable("task-manager") {
+                    TaskManager()
+                }
+                composable("quadrant") {
+                    Quadrant()
                 }
             }
         }
@@ -30,17 +48,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Article() {
+    Text("TODO")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LearningComposableTheme {
-        Greeting("Android")
+fun TaskManager() {
+    Text("TODO")
+}
+
+@Composable
+fun Quadrant() {
+    Text("TODO")
+}
+
+@Composable
+fun Root(
+    onNavigateToArticle: () -> Unit,
+    onNavigateToTaskManager: () -> Unit,
+    onNavigateToQuadrant: () -> Unit
+) {
+    Surface(Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = onNavigateToArticle) {
+                Text(text = "Article")
+            }
+            Button(onClick = onNavigateToTaskManager) {
+                Text(text = "Task Manager")
+            }
+            Button(onClick = onNavigateToQuadrant) {
+                Text(text = "Quadrant")
+            }
+        }
     }
 }
