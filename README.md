37ab8d3171aae61730651c18f5aa9ef7682f4aef
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2023-04-30 12:06:42 +0300

    feat(compose-basics): add content

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ App where I try to learn [Jetpack Compose UI App Development Toolkit](https://de
 - [Task Manager](https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems#2)
 - [Quadrant](https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems#3)
 - [Business Card](https://developer.android.com/codelabs/basic-android-kotlin-compose-business-card#0)
+- [Compose Basics](https://developer.android.com/codelabs/jetpack-compose-basics)
--- app/src/main/java/io/github/kirasok/learningcomposable/MainActivity.kt
+++ app/src/main/java/io/github/kirasok/learningcomposable/MainActivity.kt
@@ -1,14 +1,15 @@
 package io.github.kirasok.learningcomposable
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.telephony.PhoneNumberUtils
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,30 +18,36 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
@@ -52,27 +59,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "root") {
-                composable("root") {
-                    Root(
-                        { navController.navigate("article") },
-                        { navController.navigate("task-manager") },
-                        { navController.navigate("quadrant") },
-                        { navController.navigate("business-card") },
-                    )
-                }
-                composable("article") {
-                    Article()
-                }
-                composable("task-manager") {
-                    TaskManager()
-                }
-                composable("quadrant") {
-                    Quadrant()
-                }
-                composable("business-card") {
-                    BusinessCard()
+            LearningComposableTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "root") {
+                    composable("root") {
+                        Root(
+                            { navController.navigate("article") },
+                            { navController.navigate("task-manager") },
+                            { navController.navigate("quadrant") },
+                            { navController.navigate("business-card") },
+                            { navController.navigate("compose-basics") }
+                        )
+                    }
+                    composable("article") {
+                        Article()
+                    }
+                    composable("task-manager") {
+                        TaskManager()
+                    }
+                    composable("quadrant") {
+                        Quadrant()
+                    }
+                    composable("business-card") {
+                        BusinessCard()
+                    }
+                    composable("compose-basics") {
+                        ComposeBasics()
+                    }
                 }
             }
         }
@@ -249,11 +262,96 @@ fun Contact(contact: String, icon: ImageVector, contentDescriptor: String) {
 }
 
 @Composable
+fun ComposeBasics(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name -> Greeting(name = name) }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    val expanded = rememberSaveable { mutableStateOf(false) }
+    Card(
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioLowBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1F)
+            ) {
+                Text(text = "Hello,")
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold)
+                )
+                if (expanded.value) Text(
+                    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+            IconButton(onClick = { expanded.value = !expanded.value }) {
+                Icon(
+                    if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded.value) "Show less" else "Show more"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier
+                .padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
 fun Root(
     onNavigateToArticle: () -> Unit,
     onNavigateToTaskManager: () -> Unit,
     onNavigateToQuadrant: () -> Unit,
-    onNavigateToBusinessCard: () -> Unit
+    onNavigateToBusinessCard: () -> Unit,
+    onNavigateToComposeBasics: () -> Unit
 ) {
     Surface(Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
         Column(
@@ -271,6 +369,9 @@ fun Root(
             }
             Button(onClick = onNavigateToBusinessCard) {
                 Text(text = "Business Card")
+            }
+            Button(onClick = onNavigateToComposeBasics) {
+                Text(text = "Compose Basics")
             }
         }
     }
